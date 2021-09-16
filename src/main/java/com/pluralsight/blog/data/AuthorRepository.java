cd6f283cf500a3c39f98edfda790fc094691f3d1
John Bracy <john.bracy@gmail.com>
2021-09-15 21:58:51 -0500

    Removed extraneous annotations

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -2,10 +2,8 @@ package com.pluralsight.blog.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Component;
 
 import com.pluralsight.blog.model.Author;
 
-@Component
 @RepositoryRestResource(exported = false)
 public interface AuthorRepository extends JpaRepository<Author, Long> {}
